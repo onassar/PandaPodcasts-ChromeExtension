0d57dbb852a2dc62e4d843eb10a1ec165499f4ac
onassar <onassar@gmail.com>
2018-12-19 01:26:53 -0500

    Build script update for kerning

--- scripts/build.sh
+++ scripts/build.sh
@@ -33,6 +33,30 @@ mkdir build/
 shortName="crooked"
 mkdir build/$shortName/
 cp -R crx/ build/$shortName/crx/
+rm -rf build/$shortName/crx/apps/kerning/
+rm -rf build/$shortName/crx/apps/nytimes/
+rm -rf build/$shortName/crx/apps/$shortName/assets/
+rm -rf build/$shortName/crx/apps/$shortName/images/placeholders/
+rm -rf build/$shortName/crx/apps/$shortName/images/podcasts/
+rm -rf build/$shortName/crx/apps/$shortName/images/sponsors/
+cp build/$shortName/crx/manifests/$shortName.json build/$shortName/crx/manifest.json
+perl -0 -p -i -e 's/    "version": "[0-9\.]+"/    "version": "@ARGV"/' build/$shortName/crx/manifest.json $version
+rm -rf build/$shortName/crx/manifests/
+cd build/$shortName/crx/
+zip -q -r $shortName-v$version.zip .
+cd ../../../
+mv build/$shortName/crx/$shortName-v$version.zip versions/
+rm -rf build/$shortName/
+
+
+## 
+## kerning (cultures)
+## 
+## 
+shortName="kerning"
+mkdir build/$shortName/
+cp -R crx/ build/$shortName/crx/
+rm -rf build/$shortName/crx/apps/crooked/
 rm -rf build/$shortName/crx/apps/nytimes/
 rm -rf build/$shortName/crx/apps/$shortName/assets/
 rm -rf build/$shortName/crx/apps/$shortName/images/placeholders/
@@ -56,6 +80,7 @@ shortName="nytimes"
 mkdir build/$shortName/
 cp -R crx/ build/$shortName/crx/
 rm -rf build/$shortName/crx/apps/crooked/
+rm -rf build/$shortName/crx/apps/kerning/
 rm -rf build/$shortName/crx/apps/$shortName/assets/
 rm -rf build/$shortName/crx/apps/$shortName/images/placeholders/
 rm -rf build/$shortName/crx/apps/$shortName/images/podcasts/
